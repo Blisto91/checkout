cda79ab1e0cc04e56292637077a51b02605c2242
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-09-15 12:38:59 +0200

    Update git-source-provider.ts

--- src/git-source-provider.ts
+++ src/git-source-provider.ts
@@ -62,7 +62,7 @@ export async function getSource(settings: IGitSourceSettings): Promise<void> {
         stateHelper.setSafeDirectory()
       }
     }
-
+/*
     // Prepare existing directory, otherwise recreate
     if (isExisting) {
       await gitDirectoryHelper.prepareExistingDirectory(
@@ -275,7 +275,7 @@ export async function getSource(settings: IGitSourceSettings): Promise<void> {
     if (settings.submodules) {
       // Temporarily override global config
       core.startGroup('Setting up auth for fetching submodules')
-      // await authHelper.configureGlobalAuth()
+      await authHelper.configureGlobalAuth()
       core.endGroup()
 
       // Checkout submodules
@@ -295,7 +295,7 @@ export async function getSource(settings: IGitSourceSettings): Promise<void> {
         core.endGroup()
       }
     }
-
+    
     // Get commit information
     const commitInfo = await git.log1()
 
@@ -312,7 +312,7 @@ export async function getSource(settings: IGitSourceSettings): Promise<void> {
       settings.ref,
       settings.commit,
       settings.githubServerUrl
-    )
+    )*/
   } finally {
     // Remove auth
     if (authHelper) {
